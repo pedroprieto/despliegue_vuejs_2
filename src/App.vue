cc59822f4f7b0be4c5711b43d12250fb1bd9cf46
Pedro Prieto <pedpral@gmail.com>
2025-02-26 20:44:25 +0100

    Despedida

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-	  <h1>Pedro Prieto</h1>
+	  <h1>Ha sido un placer teneros como alumn@s</h1>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
